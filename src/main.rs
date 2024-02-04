9fbd8c3f36e1a3520c4e41a1c721545f6127b29a
danyrey <danyrey@gmx.net>
2024-02-04 16:52:31 +0100

    start screen first draft
    example code from the bevy examples state.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    StartScreen,
+    MissionObjectives(MissionObjectives),
+    DeployScreen(DeployScreen),
+    Match,
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum MissionObjectives {
+    #[default]
+    Missions,
+    EditMissions, // how to remove redundancy as this screen exists also in MissionObjectives
+    Upgrades,
+    LocationObjectives,
+    Notes,
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum DeployScreen {
+    #[default]
+    ChooseLocation,
+    ActiveMissions,
+    EditMissions, // how to remove redundancy as this screen exists also in MissionObjectives
+    ActiveDutyConfirmation,
+    EditLoadout, // how to remove redundancy as this screen exists also in MissionObjectives
+    MatchMake,
 }
 
 #[derive(Component)]
@@ -18,15 +44,119 @@ fn add_people(mut commands: Commands) {
 
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in &query {
-        println!("hello {}!", name.0);
+        debug!("hello {}!", name.0);
     }
 }
 
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+// --- Start Screen START
+
+pub struct StartScreenPlugin;
+
+impl Plugin for StartScreenPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(AppState::StartScreen), start_start_screen)
+            .add_systems(
+                Update,
+                (update_start_screen).run_if(in_state(AppState::StartScreen)),
+            )
+            .add_systems(OnExit(AppState::StartScreen), bye_start_screen);
+    }
+}
+
+#[derive(Resource)]
+struct MenuData {
+    button_entity: Entity,
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn start_start_screen(mut commands: Commands) {
+    debug!("starting start screen");
+    let button_entity = commands
+        .spawn(NodeBundle {
+            style: Style {
+                // center button
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.),
+                        height: Val::Px(65.),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Play",
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                            ..default()
+                        },
+                    ));
+                });
+        })
+        .id();
+    commands.insert_resource(MenuData { button_entity });
+}
+
+fn update_start_screen(
+    mut next_state: ResMut<NextState<AppState>>,
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+) {
+    debug!("updating start screen");
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = PRESSED_BUTTON.into();
+                next_state.set(AppState::DeployScreen(DeployScreen::ChooseLocation));
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn bye_start_screen(mut commands: Commands, menu_data: Res<MenuData>) {
+    debug!("bye start screen!");
+    commands.entity(menu_data.button_entity).despawn_recursive();
+}
+
+// --- Start Screen STOP
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, StartScreenPlugin))
+        .add_state::<AppState>()
         .add_systems(Update, bevy::window::close_on_esc)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_systems(Startup, (add_people, setup))
+        //.add_systems(Update, greet_people)
         .run();
 }
