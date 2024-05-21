d8cc1ce482ae45bffb7940658153aa79455aa8d7
danyrey <danyrey@gmx.net>
2024-05-21 23:40:55 +0200

    added the infil countdown

--- src/raid.rs
+++ src/raid.rs
@@ -8,27 +8,84 @@ use crate::AppState::{self, Raid};
 
 // Events
 
-#[derive(Event)]
+#[derive(Event, Debug)]
 struct InfilCounter(u32);
+
+#[derive(Event)]
+struct InfilComplete;
+
+// Resources
+
+#[derive(Default, Resource)]
+struct InfilCountdown {
+    counter: u32,
+}
+
+// Plugin
 
 pub struct RaidPlugin;
 
 impl Plugin for RaidPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(Raid), start_raid)
-            .add_systems(Update, (update_raid).run_if(in_state(Raid)))
+        app.add_event::<InfilCounter>()
+            .add_event::<InfilComplete>()
+            .add_systems(OnEnter(Raid), start_raid)
+            .add_systems(
+                Update,
+                (
+                    update_raid,
+                    infil_countdown_listener,
+                    infil_countdown_complete_listener,
+                )
+                    .run_if(in_state(Raid)),
+            )
+            .add_systems(FixedUpdate, (infil_countdown).run_if(in_state(Raid)))
             .add_systems(OnExit(Raid), bye_raid);
     }
 }
 
+// Systems
+
 fn start_raid(mut commands: Commands) {
-    todo!("raid setup system")
+    debug!("starting raid called");
+    commands.insert_resource(InfilCountdown { counter: 31 });
 }
 
-fn update_raid(mut next_state: ResMut<NextState<AppState>>) {
-    todo!("raid update system")
+fn update_raid(mut _next_state: ResMut<NextState<AppState>>) {
+    debug!("updating raid called");
 }
 
-fn bye_raid(mut commands: Commands) {
-    todo!("raid exit system")
+fn bye_raid(mut _commands: Commands) {
+    debug!("exiting raid called");
+}
+
+fn infil_countdown(
+    mut infil_countdown: ResMut<InfilCountdown>,
+    mut event: EventWriter<InfilCounter>, // new ping we are currently searching for
+) {
+    debug!("infil countdown system called");
+    if infil_countdown.counter > 0 {
+        infil_countdown.counter -= 1;
+        event.send(InfilCounter(infil_countdown.counter));
+        debug!("infil countdown event send : {:?}", infil_countdown.counter);
+    }
+}
+fn infil_countdown_listener(
+    mut events: EventReader<InfilCounter>,
+    mut complete: EventWriter<InfilComplete>,
+) {
+    for event in events.read() {
+        // TODO: print countdown on screen, output just to log for now
+        debug!("counter received: {:?}", event);
+        if event.0 == 0 {
+            complete.send(InfilComplete);
+            debug!("sending complete event");
+        }
+    }
+}
+
+fn infil_countdown_complete_listener(mut events: EventReader<InfilComplete>) {
+    for _ in events.read() {
+        todo!("'activate' controls so player can move now. match is in progress now")
+    }
 }
