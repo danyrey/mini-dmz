2b98ffa3b14a786d66c7c05bc5b02082a4fabd42
danyrey <danyrey@gmx.net>
2024-05-26 11:16:10 +0200

    added a sample scene to look at

--- src/fake_level.rs
+++ src/fake_level.rs
@@ -16,22 +16,69 @@ impl Plugin for FakeLevelPlugin {
 }
 
 // Components
+#[derive(Component)]
+struct FakeLevelStuff;
 
 // Resources
 
 // Events
-
-fn start_fake_level(mut _commands: Commands) {
-    // TODO: some cubes and planes
+fn start_fake_level(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     debug!("starting fake level");
+    // circular base
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Circle::new(8.0)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                ..Default::default()
+            }),
+            transform: Transform::from_rotation(Quat::from_rotation_x(
+                -std::f32::consts::FRAC_PI_2,
+            )),
+            ..default()
+        })
+        .insert(Name::new("Disc"))
+        .insert(FakeLevelStuff);
+    // cube
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::GOLD,
+                ..Default::default()
+            }),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Name::new("Cuboid"))
+        .insert(FakeLevelStuff);
+    // light
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        })
+        .insert(Name::new("PointyLight"))
+        .insert(FakeLevelStuff);
 }
+
 fn update_fake_level() {
     // TODO: maybe just render them near any cameras
     // TODO: maybe put code here that moves the scene near cameras to maintain a reference for
     // movement
     debug!("updating fake level");
 }
-fn bye_fake_level(mut _commands: Commands) {
-    // TODO: cleanup scene
+fn bye_fake_level(mut commands: Commands, query: Query<Entity, With<FakeLevelStuff>>) {
     debug!("stopping fake level");
+    for entity in &query {
+        commands.entity(entity).despawn_recursive();
+    }
 }
--- src/raid.rs
+++ src/raid.rs
@@ -66,6 +66,7 @@ fn start_raid(mut commands: Commands) {
             transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         })
+        .insert(Name::new("FirstPersonCamera"))
         .insert(FirstPersonCamera);
 }
 
