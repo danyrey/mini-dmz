f88c58d845dadd583e568096054ef878054889f1
danyrey <danyrey@gmx.net>
2024-08-05 20:50:58 +0200

    additions to damage system
    - damage component
    - test module
    - added test module template to template plugin

--- src/damage.rs
+++ src/damage.rs
@@ -1,6 +1,8 @@
 use bevy::app::Plugin;
 use bevy::math::bounding::{Aabb3d, BoundingSphere};
 
+use crate::armor::Armor;
+use crate::health::Health;
 use crate::AppState;
 use crate::AppState::Raid;
 use bevy::prelude::*;
@@ -39,6 +41,10 @@ pub struct HitBox(pub CollisionVolume);
 #[derive(Component, Debug)]
 pub struct HurtBox(pub CollisionVolume);
 
+/// damage component
+#[derive(Component, Debug)]
+pub struct Damage(u8);
+
 // Resources
 
 // Events
@@ -61,15 +67,21 @@ fn start_damage_system(mut _commands: Commands) {
 
 fn update_damage_system(
     // TODO: query the components to which to decide damage events
-    mut hitbox_query: Query<(Entity, &GlobalTransform, &HitBox)>,
-    mut hurtbox_query: Query<(Entity, &GlobalTransform, &HurtBox)>,
+    mut hitbox_query: Query<(Entity, &GlobalTransform, &HitBox, &Damage)>,
+    mut hurtbox_query: Query<(
+        Entity,
+        &GlobalTransform,
+        &HurtBox,
+        Option<&Health>,
+        Option<&Armor>,
+    )>,
     mut _health: EventWriter<HealthDamageReceived>,
     mut _armor: EventWriter<ArmorDamageReceived>,
 ) {
     debug!("updating {}", NAME);
     // TODO: check for colliding hurt/hitbox combinations
-    for (_hit_entity, _hit_transform, _hitbox) in hitbox_query.iter_mut() {
-        for (_hurt_entity, _hurt_transform, _hurtbox) in hurtbox_query.iter_mut() {
+    for (_hit_entity, _hit_transform, _hitbox, _damage) in hitbox_query.iter_mut() {
+        for (_hurt_entity, _hurt_transform, _hurtbox, _health, _armor) in hurtbox_query.iter_mut() {
             // dont hit yourself if overlap occours
             if _hit_entity != _hurt_entity {
                 // TODO: check for overlap and produce event
@@ -83,3 +95,26 @@ fn bye_damage_system(mut _commands: Commands) {
 }
 
 // helper functions
+// tests
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn should_test_something() {
+        // given
+        let mut app = App::new();
+
+        // when
+        //app.add_event::<HealthDamageReceived>();
+        //app.add_systems(Update, damage_received_listener);
+        //let entity = app.borrow_mut().world.spawn(Health(100)).id();
+        //app.borrow_mut().world.resource_mut::<Events<HealthDamageReceived>>().send(HealthDamageReceived { entity, damage: 10 });
+        //app.update();
+
+        // then
+        //assert!(app.world.get::<Health>(entity).is_some());
+        //assert_eq!(app.world.get::<Health>(entity).unwrap().0, 90);
+    }
+}
--- src/template_plugin.rs
+++ src/template_plugin.rs
@@ -39,3 +39,27 @@ fn bye_template_system(mut _commands: Commands) {
 }
 
 // helper functions
+
+// tests
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn should_test_something() {
+        // given
+        let mut app = App::new();
+
+        // when
+        //app.add_event::<HealthDamageReceived>();
+        //app.add_systems(Update, damage_received_listener);
+        //let entity = app.borrow_mut().world.spawn(Health(100)).id();
+        //app.borrow_mut().world.resource_mut::<Events<HealthDamageReceived>>().send(HealthDamageReceived { entity, damage: 10 });
+        //app.update();
+
+        // then
+        //assert!(app.world.get::<Health>(entity).is_some());
+        //assert_eq!(app.world.get::<Health>(entity).unwrap().0, 90);
+    }
+}
