87f712af981271ad21ddc117675b78018ac5e685
danyrey <danyrey@gmx.net>
2024-09-23 20:30:55 +0200

    preparing migration to 0.14, close_on_esc

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,23 @@ fn main() {
             InventoryTestingPlugin,
         ))
         .init_state::<AppState>()
-        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Update, close_on_esc)
         .add_systems(Startup, setup)
         .run();
+}
+
+pub fn close_on_esc(
+    mut commands: Commands,
+    focused_windows: Query<(Entity, &Window)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    for (window, focus) in focused_windows.iter() {
+        if !focus.focused {
+            continue;
+        }
+
+        if input.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
 }
